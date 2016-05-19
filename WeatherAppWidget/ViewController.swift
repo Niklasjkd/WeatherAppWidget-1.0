66ed570ea8fb97339c83cea2cedc1bdce44c1cd6
Niklas Düttmann <niklas.duettmann033@gmail.com>
2016-05-19 23:14:32 +0200

    Initial Commit

---
+++ WeatherAppWidget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAppWidget
+//
+//  Created by Niklas Düttmann on 19.05.16.
+//  Copyright © 2016 Niklas Düttmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
